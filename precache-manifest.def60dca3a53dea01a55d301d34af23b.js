4249603c904748017b6a15612f019c8243969d33
Tomas <tomas87@gmail.com>
2020-01-08 13:41:46 +0100

    Updates

---
+++ precache-manifest.def60dca3a53dea01a55d301d34af23b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a560693cc2dc7fe815652107972adaa7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe0f292709f382a894cc",
+    "url": "/monsters-rolodex/static/css/main.ffec971b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fe0f292709f382a894cc",
+    "url": "/monsters-rolodex/static/js/main.e8a99716.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
